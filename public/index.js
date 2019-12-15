5d87d8f5d8cca97d5bf5dfb1971f4957f14ba162
eschu1 <eschu1@uw.edu>
2019-12-15 14:42:41 -0800

    Client files created

---
+++ public/index.js
@@ -0,0 +1,41 @@
+/**
+ */
+"use strict";
+(function() {
+
+  window.addEventListener("load", init);
+
+  function init() {
+  }
+
+  /**
+   * Returns the element that has the ID attribute with the specified value.
+   * @param {string} name - element ID.
+   * @returns {object} - DOM object associated with id.
+   */
+  function id(name) {
+    return document.getElementById(name);
+  }
+
+  /**
+   * Returns the first element that matches the given CSS selector.
+   * @param {string} query - CSS query selector.
+   * @returns {object[]} array of DOM objects matching the query.
+   */
+  function qs(query) {
+    return document.querySelector(query);
+  }
+
+  /**
+   * Returns the array of elements that match the given CSS selector.
+   * @param {string} query - CSS query selector
+   * @returns {object[]} array of DOM objects matching the query.
+   */
+  function qsa(query) {
+    return document.querySelectorAll(query);
+  }
+
+  function gen(tagName) {
+    return document.createElement(tagName);
+  }
+})();
